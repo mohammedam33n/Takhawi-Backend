5d4dfc901c90676cbce9bf2d6f3a746150fd9d3c
mohammedam33n <mohammedam33n.com>
2023-09-25 18:39:35 +0300

    configuration socket io in the local

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/test-socket', function () {
+    return view('test-socket');
+});
+
+
+
